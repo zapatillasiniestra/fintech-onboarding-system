db90e64d3c9480fa7bb93e67008de1cc952d5ad4
funkafari <funkafari@users.noreply.github.com>
2026-06-20 21:36:00 +0400

    Updated src/server.js with env.PORT

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
- 
