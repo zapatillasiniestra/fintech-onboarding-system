f8de33d6d7246bb84e28274074843c786e43e044
funkafari <funkafari@users.noreply.github.com>
2026-06-22 00:54:16 +0400

    Clean Prisma files and create DB for Render deployment.

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,11 @@
+require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
