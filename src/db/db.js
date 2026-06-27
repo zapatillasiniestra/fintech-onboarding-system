91c6d4ca643769593605a913dd6997c12d0fe886
zapatillasiniestra <zapatillasiniestra@users.noreply.github.com>
2026-06-27 23:50:47 +0400

    test.yml and db.js have been fixed.

--- src/db/db.js
+++ src/db/db.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
