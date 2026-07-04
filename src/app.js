53580cb407f0b35afd31eb3358306245ebe20a75
zapatillasiniestra <zapatillasiniestra@users.noreply.github.com>
2026-07-04 23:52:37 +0400

    Removed adminRoutes in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const authRoutes = require("./routes/auth.routes");
 const applicationsRoutes = require("./routes/applications.routes");
-const adminRoutes = require("./routes/admin.routes");
+// const adminRoutes = require("./routes/admin.routes");
 const errorHandler = require("./middleware/error.middleware");
 const requestLogger = require("./middleware/logger.middleware");
 const healthRoutes = require("./routes/health.routes");
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(requestLogger);
 
 app.use("/", authRoutes);
-app.use("/", adminRoutes);
+// app.use("/", adminRoutes);
 app.use("/applications", applicationsRoutes);
 app.use("/", healthRoutes);
 
