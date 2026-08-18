73bb1d2c566130554663503a6e56bcb48e0e92f8
zapatillasiniestra <zapatillasiniestra@users.noreply.github.com>
2026-08-18 22:21:34 +0200

    rename project to Nahuel, continue

--- src/swagger.ts
+++ src/swagger.ts
@@ -4,9 +4,9 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Fintech Onboarding API",
+      title: "Nahuela API",
       version: "1.0.0",
-      description: "REST API for onboarding applications"
+      description: "Open-source API for auditable onboarding and regulated decision systems."
     },
     servers: [
       {
