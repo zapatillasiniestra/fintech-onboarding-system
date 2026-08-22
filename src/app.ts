62c019e9b2d2e3d77aed1f7ca71ad5675f6f8f63
zapatillasiniestra <zapatillasiniestra@users.noreply.github.com>
2026-08-22 22:47:02 +0200

    configure production  CORS

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://nahuela-web.onrender.com",
+    ],
   })
 );
 
